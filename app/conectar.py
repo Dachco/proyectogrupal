82a3a1bfbbe3d895972e6abae37773a8b4983275
Arcxno <williamfonsecapardo@gmail.com>
2024-05-30 16:14:10 -0500

    requerimientos y modificacion en el main.py

--- app/conectar.py
+++ app/conectar.py
@@ -1,8 +1,27 @@
 import mysql.connector
+import pyodbc
 
-database = mysql.connector.connect(
-    host='sql10.freemysqlhosting.net',
-    user='sql10708457',
-    password='ccaDWZLwMF',
-    database='sql10708457'
-)
+# Conexión a MySQL
+#mysql_connection = mysql.connector.connect(
+#    host='sql10.freemysqlhosting.net',
+#    user='sql10708457',
+#    password='ccaDWZLwMF',
+#    database='sql10708457'
+#)
+
+# Detalles de la conexión
+server = 'LAPTOP-UE98O5QF'
+database = 'ProyectoTienda'  # Nombre de la base de datos
+
+# Configura la cadena de conexión para autenticación de Windows y deshabilitar la verificación del certificado SSL
+connection_string = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;TrustServerCertificate=yes;'
+
+try:
+    # Establece la conexión
+    sql_server_connection = pyodbc.connect(connection_string)
+    print("Conexión exitosa")
+
+except pyodbc.Error as ex:
+    print(f"Error al conectar a SQL Server: {ex}")
+
+
