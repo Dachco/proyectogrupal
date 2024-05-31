d8d7cacab6ca8956ae1f08a16210b25afcc192b7
Arcxno <williamfonsecapardo@gmail.com>
2024-05-31 10:08:35 -0500

    segundo comit y script

--- app/conectar.py
+++ app/conectar.py
@@ -1,13 +1,4 @@
-import mysql.connector
 import pyodbc
-
-# Conexión a MySQL
-#mysql_connection = mysql.connector.connect(
-#    host='sql10.freemysqlhosting.net',
-#    user='sql10708457',
-#    password='ccaDWZLwMF',
-#    database='sql10708457'
-#)
 
 # Detalles de la conexión
 server = 'LAPTOP-UE98O5QF'
